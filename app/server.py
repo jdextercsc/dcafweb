e69d9364828815fa390cfab881f8d5ccee506eb5
wgd3 <wdaniel5@csc.com>
2015-10-22 15:58:53 -0400

    Added test hosts and more styling

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from flask import Flask, render_template, url_for, g, session, flash, redirect
+from flask import Flask, render_template, url_for, g, session, flash, redirect, request
 # from flask.ext import restful
 # from flask.ext.restful import reqparse, Api
 # from flask.ext.sqlalchemy import SQLAlchemy
@@ -12,6 +12,7 @@ basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../')
 
 app = Flask(__name__)
 app.config.from_object('app.config')
+app.secret_key = '"*\x995\x9a\xf9\xf2\x13z\xd3\x9c=\x18u\xef\xf8\x90WD\xa3\xe5\xe1\x19\xb7'
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -77,9 +78,14 @@ def load_session_from_cookie():
             g.hanlonDefined = True
             logging.debug("Hanlon URL is defined, rendering tasks...")
             logging.debug("URL: %s", session['hanlonURL'])
+            g.hanlonURL = session['hanlonURL']
     else:
+        # check to see if we're navigating to a step without the API defined
+        if request.path != '/':
+            flash("Hanlon API endpoint not defined.", "warning")
+            logging.debug("Request path: %s", str(request.path))
         g.hanlonDefined = False
-        logging.debug("Handlon URL is not defined, rendering landing page...")
+        logging.debug("Handlon URL is not defined!")
 
     g.deploy_steps = DEPLOY_STEPS
 
@@ -88,16 +94,60 @@ def load_session_from_cookie():
 def home():
     if g.hanlonDefined:
         flash('Hanlon API Endpoint already defined', 'warning')
+    else:
+        pass
     return render_template('hanlonconnect.html')
 
 @app.route('/hosts')
-def hosts():
-    render_template('hostselect.html')
+@app.route('/hosts/<debug>')
+def hosts(debug=None):
+    if debug == 'debug':
+        test_hosts = [
+            {
+                'hostname': 'controller1',
+                'ipmi_ip': '10.0.0.1',
+                'mgmt_ip': '172.0.0.1',
+                'disks': ['disk1', 'disk2', 'disk3']
+            },
+            {
+                'hostname': 'controller2',
+                'ipmi_ip': '10.0.0.2',
+                'mgmt_ip': '172.0.0.2',
+                'disks': ['disk1', 'disk2', 'disk3']
+            },
+            {
+                'hostname': 'controller3',
+                'ipmi_ip': '10.0.0.3',
+                'mgmt_ip': '172.0.0.3',
+                'disks': ['disk1', 'disk2', 'disk3']
+            },
+            {
+                'hostname': 'compute1',
+                'ipmi_ip': '10.0.0.4',
+                'mgmt_ip': '172.0.0.4',
+                'disks': ['disk1', 'disk2', 'disk3']
+            },
+            {
+                'hostname': 'compute2',
+                'ipmi_ip': '10.0.0.5',
+                'mgmt_ip': '172.0.0.5',
+                'disks': ['disk1', 'disk2', 'disk3']
+            },
+            {
+                'hostname': 'compute3',
+                'ipmi_ip': '10.0.0.6',
+                'mgmt_ip': '172.0.0.6',
+                'disks': ['disk1', 'disk2', 'disk3']
+            },
+        ]
+        return render_template('hostselect.html', hosts = test_hosts)
+
+    return render_template('hostselect.html')
 
 @app.route('/group_vars')
 def group_vars():
-    pass
+    return render_template('group_vars.html')
 
 @app.route('/host_vars')
 def host_vars():
-    pass
+    return render_template('host_vars.html')
